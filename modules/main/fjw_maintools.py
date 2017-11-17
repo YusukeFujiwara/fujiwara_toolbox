723aae496b754aaabbb03e664f06834b37f3e49f
YusukeFujiwara <ysk.sanekata@gmail.com>
2017-11-18 05:25:22 +0900

    複数シミュ成功

--- modules/main/fjw_maintools.py
+++ modules/main/fjw_maintools.py
@@ -15335,7 +15335,8 @@ class MDObject():
     def export_abc(self, dirpath):
         self.__export_setup(dirpath)
         path = os.path.normpath(self.export_dir + os.sep + self.mdname + ".abc")
-        bpy.ops.wm.alembic_export(filepath=path, start=1, end=10, selected=True, visible_layers_only=True, flatten=True, apply_subdiv=True, compression_type='OGAWA')
+        print("export abc:%s"%path)
+        bpy.ops.wm.alembic_export(filepath=path, start=1, end=10, selected=True, visible_layers_only=True, flatten=True, apply_subdiv=True, compression_type='OGAWA', as_background_job=False)
 
     def export_to_mddata(self):
         self.export_abc(self.export_dir)
